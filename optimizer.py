a51b1b77ca1c8da300f1981df166d34fc06d4a6f
liminovna <vladaliming@gmail.com>
2025-12-08 01:41:51 +0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -35,8 +35,10 @@ class AdamW(Optimizer):
 
             # TODO: Clip gradients if max_grad_norm is set
             if group['max_grad_norm'] is not None:
-                raise NotImplementedError()
-            
+                params_with_grad = [p for p in group["params"] if p.grad is not None]
+                grads = [p.grad for p in params_with_grad]
+                torch.nn.utils.clip_grad_norm_(params_with_grad, group['max_grad_norm'])
+
             for p in group["params"]:
                 if p.grad is None:
                     continue
@@ -44,23 +46,36 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
-
                 # State should be stored in this dictionary
                 state = self.state[p]
+                if len(state) == 0:
+                    state["m"] = torch.zeros_like(p.data)
+                    state["v"] = torch.zeros_like(p.data)
+                    state["t"] = 0
+
+                state["t"] += 1
 
                 # TODO: Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                t = state["t"]
+                eps, weight_decay = group["eps"], group["weight_decay"]
+                beta1, beta2 = group["betas"][0], group["betas"][1]
 
                 # TODO: Update first and second moments of the gradients
+                state["m"] = beta1 * state["m"] + (1 - beta1) * grad
+                state["v"] = beta2 * state["v"] + (1 - beta2) * (grad ** 2)
 
                 # TODO: Bias correction
-                # Please note that we are using the "efficient version" given in Algorithm 2 
+                # Please note that we are using the "efficient version" given in Algorithm 2
                 # https://arxiv.org/pdf/1711.05101
+                step_size = alpha * ((1 - beta2 ** t) ** 0.5) / (1 - beta1 ** t)
 
                 # TODO: Update parameters
+                p.data.addcdiv_(state["m"], state["v"].sqrt().add_(eps) ,value=-step_size)
 
                 # TODO: Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+                if group.get("weight_decay", 0) != 0:
+                    p.data.add_(p.data, alpha=-alpha * weight_decay)
 
         return loss
